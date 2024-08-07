6f798ec006852398f5ebd554442dc94be82aad02
Nabila Ayaba <nabilaayaba@gmail.com>
2024-08-07 16:17:48 +0300

    Add spotlight

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+let prevStudent
+
 document.getElementById('select-student').addEventListener('click', () => {
-    const seats = document.querySelectorAll('.seat:not(.strike)');
-    if (seats.length === 0) {
-        alert('All students have been called!');
-        return;
-    }
+  const seats = document.querySelectorAll('.seat:not(.strike)')
+  if (seats.length === 0) {
+    alert('All students have been called!')
+    return
+  }
 
-    const randomIndex = Math.floor(Math.random() * seats.length);
-    const selectedStudent = seats[randomIndex];
-    const studentName = selectedStudent.innerText;
+  const randomIndex = Math.floor(Math.random() * seats.length)
+  const selectedStudent = seats[randomIndex]
+  const studentName = selectedStudent.innerText
 
-    document.getElementById('random-student').innerText = studentName;
-    selectedStudent.classList.add('strike');
-});
+  if (prevStudent) {
+    prevStudent.classList.remove('current')
+    prevStudent.classList.add('strike')
+  }
+  prevStudent = selectedStudent
+  prevStudent.classList.add('current')
+
+  document.getElementById('random-student').innerText = studentName
+})
